307826fa1677d3e56b5a363b64391e1040d766a1
Seema25dhami <seemadhamiwal2001@gmail.com>
2023-04-09 00:52:30 +0530

    user model and user info file updates

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,19 +1,24 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:phonelogin/models/user_model.dart';
 import 'package:phonelogin/screens/otp_screen.dart';
 import 'package:phonelogin/utils/utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-class AuthProvider extends ChangeNotifier {
 
+class AuthProvider extends ChangeNotifier {
   bool _isSignedIn = false;
   bool get isSignedIn => isSignedIn;
   bool _isLoading = false;
   bool get isLoading => isLoading;
   String? _uid;
   String get uid => _uid!;
-  final FirebaseAuth _firebaseAuth=FirebaseAuth.instance;
-  final FirebaseFirestore _firebaseFirestore=FirebaseFirestore.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
 
   AuthProvider() {
     checkSignIn();
@@ -28,62 +33,91 @@ class AuthProvider extends ChangeNotifier {
   void signInWithPhone(BuildContext context, String phoneNumber) async {
     try {
       await _firebaseAuth.verifyPhoneNumber(
-        phoneNumber: phoneNumber,
-          verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async{
+          phoneNumber: phoneNumber,
+          verificationCompleted:
+              (PhoneAuthCredential phoneAuthCredential) async {
             await _firebaseAuth.signInWithCredential(phoneAuthCredential);
           },
-          verificationFailed: (error){
+          verificationFailed: (error) {
             throw Exception(error.message);
           },
-          codeSent: (verificationId,forceResendingToken){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>OtpScreen(verificationId: verificationId)));
-
-
-
+          codeSent: (verificationId, forceResendingToken) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        OtpScreen(verificationId: verificationId)));
           },
-          codeAutoRetrievalTimeout: (verificationId){});
+          codeAutoRetrievalTimeout: (verificationId) {});
     } on FirebaseAuthException catch (e) {
       showSnackBar(context, e.message.toString());
-
     }
   }
-  void  verifyOtp({
-      required BuildContext context,
+
+  void verifyOtp(
+      {required BuildContext context,
       required String verificationid,
       required String user0tp,
-      required Function onSuccess})async{
-    _isLoading=true;
+      required Function onSuccess}) async {
+    _isLoading = true;
     notifyListeners();
     try {
-      PhoneAuthCredential creds=PhoneAuthProvider.credential(verificationId: verificationid, smsCode: user0tp);
-      User? user=(await _firebaseAuth.signInWithCredential(creds)).user!;
-      if(user!=null){
-        _uid=user.uid;
+      PhoneAuthCredential creds = PhoneAuthProvider.credential(
+          verificationId: verificationid, smsCode: user0tp);
+      User? user = (await _firebaseAuth.signInWithCredential(creds)).user!;
+      if (user != null) {
+        _uid = user.uid;
         onSuccess();
-
       }
-      _isLoading=false;
+      _isLoading = false;
       notifyListeners();
-
     } on FirebaseAuthException catch (e) {
-      showSnackBar (context, e.message.toString());
-      _isLoading=false;
+      showSnackBar(context, e.message.toString());
+      _isLoading = false;
       notifyListeners();
     }
-
   }
+
   //database operations--> saving data of our user
   Future<bool> checkExistingUser() async {
-  DocumentSnapshot snapshot =
-  await _firebaseFirestore.collection("users").doc (_uid).get();
-  if (snapshot.exists) {
-  print("USER EXISTS");
-  return true;
-  } else {
-  print("NEW USER");
+    DocumentSnapshot snapshot =
+        await _firebaseFirestore.collection("users").doc(_uid).get();
+    if (snapshot.exists) {
+      print("USER EXISTS");
+      return true;
+    } else {
+      print("NEW USER");
       return false;
+    }
+  }
+
+  void saveUserDataToFirebase({
+    required BuildContext context,
+    required UserModel userModel,
+    required File profilePic,
+    required onSuccess,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+    try {
+      // uploading images to firebase storgae
+      await storeFilToSTorage("profilePic/$_uid", profilePic).then((value) {
+        userModel.profilePic = value;
+        userModel.createdAt = DateTime.now().millisecondsSinceEpoch.toString();
+        userModel.phoneNumber = _firebaseAuth.currentUser!.phoneNumber!;
+        userModel.uid = _firebaseAuth.currentUser!.phoneNumber!;
+      });
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message.toString());
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<String> storeFilToSTorage(String ref, File file) async {
+    UploadTask uploadTask = _firebaseStorage.ref().child(ref).putFile(file);
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
   }
 }
-}
-
-
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:phonelogin/provider/auth_provider.dart';
+import 'package:phonelogin/screens/welcome_screen.dart';
+import 'package:provider/provider.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -9,14 +13,47 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text("App Auth"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ap.userSignOut().then(
+                    (value) => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WelcomeScreen(),
+                      ),
+                    ),
+                  );
+            },
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.purple,
+            backgroundImage: NetworkImage(ap.userModel.profilePic),
+            radius: 50,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(ap.userModel.name),
+          Text(ap.userModel.email),
+          Text(ap.userModel.phoneNumber),
+          Text(ap.userModel.bio),
+        ],
+      )),
+    );
   }
 }
-
-
-
-
-
-
-
-
